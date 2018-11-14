bcd2b4dda9509b1fa0fcea55610d58e64b773ee3
birkin <birkin.diana@gmail.com>
2018-11-14 16:19:37 -0500

    initial commit.

---
+++ app_x/views.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+import datetime, json, logging, os, pprint
+from . import settings_app
+from app_x.lib import view_info_helper
+# from app_x.lib.shib_auth import shib_login  # decorator
+from django.conf import settings as project_settings
+from django.contrib.auth import logout
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+
+
+log = logging.getLogger(__name__)
+
+
+def info( request ):
+    """ Returns basic data including branch & commit. """
+    1/0  # tests admin error-emails (remove) -- view output via running, in another terminal window: `python -m smtpd -n -c DebuggingServer localhost:1026`
+    # log.debug( 'request.__dict__, ```%s```' % pprint.pformat(request.__dict__) )
+    rq_now = datetime.datetime.now()
+    commit = view_info_helper.get_commit()
+    branch = view_info_helper.get_branch()
+    info_txt = commit.replace( 'commit', branch )
+    resp_now = datetime.datetime.now()
+    taken = resp_now - rq_now
+    context_dct = view_info_helper.make_context( request, rq_now, info_txt, taken )
+    output = json.dumps( context_dct, sort_keys=True, indent=2 )
+    return HttpResponse( output, content_type='application/json; charset=utf-8' )
+
+
+# @shib_login
+# def login( request ):
+#     """ Handles authNZ, & redirects to admin.
+#         Called by click on login or admin link. """
+#     next_url = request.GET.get( 'next', None )
+#     if not next_url:
+#         redirect_url = reverse( settings_app.POST_LOGIN_ADMIN_REVERSE_URL )
+#     else:
+#         redirect_url = request.GET['next']  # will often be same page
+#     log.debug( 'redirect_url, ```%s```' % redirect_url )
+#     return HttpResponseRedirect( redirect_url )
---
+++ config/urls.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.views.generic import RedirectView
+from app_x import views
+
+
+admin.autodiscover()
+
+
+urlpatterns = [
+
+    # url( r'^admin/login/', RedirectView.as_view(pattern_name='login_url') ),
+
+    url( r'^admin/', admin.site.urls ),  # eg host/project_x/admin/
+
+    url( r'^info/$', views.info, name='info_url' ),
+
+    # url( r'^login/$', views.login, name='login_url' ),
+
+    url( r'^$', RedirectView.as_view(pattern_name='info_url') ),
+
+    ]
