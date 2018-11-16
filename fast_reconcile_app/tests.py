f9c587a099cb2efa4bfd367019bfa0a883aa4453
birkin <birkin.diana@gmail.com>
2018-11-16 16:26:07 -0500

    adds massage-query tests.

--- fast_reconcile_app/tests.py
+++ fast_reconcile_app/tests.py
@@ -2,10 +2,11 @@
 
 import logging
 from django.test import TestCase
-# from django.test import SimpleTestCase as TestCase    ## TestCase requires db, so if you're not using a db, and want tests, try this
+from fast_reconcile_app.lib import query_parser
 
 
 log = logging.getLogger(__name__)
+
 TestCase.maxDiff = None
 
 
@@ -26,4 +27,32 @@ class RootUrlTest( TestCase ):
         redirect_url = response._headers['location'][1]
         self.assertEqual(  '/info/', redirect_url )
 
-    # end class RootUrlTest()
+    ## end class RootUrlTest()
+
+
+class QueryParserTest( TestCase ):
+    """ Checks aspects of the perceived query. """
+
+    def test_static_query(self):
+        """ Checks basic queries that should not be changed. """
+        self.assertEqual(
+            'feminism',
+            query_parser.massage_query( 'feminism' )
+            )
+        self.assertEqual(
+            'Particles (physics)',
+            query_parser.massage_query( 'Particles (physics)' )
+            )
+
+    def test_massaged_query(self):
+        """ Checks queries that should be transformed. """
+        self.assertEqual(
+            'Particles Nuclear physics charm',
+            query_parser.massage_query( 'Particles (Nuclear physics)' )
+            )
+        self.assertEqual(
+            'zParticles (Nuclear physics charm)',
+            query_parser.massage_query( 'Particles (Nuclear physics charm)' )
+            )
+
+    ## end class QueryParserTest()
